53e209e9b421e3826ade0d924ebd9b1b288e641a
gaunt0066 <gaunt0066@gmail.com>
2022-09-06 10:34:45 +0300

    made cats

---
+++ cat-me.js
@@ -0,0 +1,7 @@
+
+ catMe = require('cat-me')
+ for(let i =4;i>0;i--){
+console.log(catMe())
+
+
+ }
---
+++ elGato.js
@@ -0,0 +1,6 @@
+let catMe = require('cat-me')
+ for(let i =3;i>0;i--){
+console.log(catMe())
+
+
+ }
